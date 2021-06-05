866c3a22ed1005a1770a496f1b90470b0da16e4a
Carol Brandes <carolbrandes23@gmail.com>
2021-06-05 12:52:05 -0300

    Feed - verifica se o usuario esta autenticado

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
 import { Container } from "reactstrap";
 export default () => {
+  useEffect(() => {
+    if (
+      !window.localStorage.getItem("username") ||
+      !window.localStorage.getItem("token")
+    ) {
+      window.location.href = "/";
+    }
+  }, []);
   return (
     <Container>
       <h1>Feed</h1>
