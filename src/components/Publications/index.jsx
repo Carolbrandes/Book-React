d76da403c00144a10308f63143c35faa9cfaea63
Carol <carolbrandes23@gmail.com>
2021-06-05 19:38:56 -0300

    feed- coluna de publicacoes

--- src/components/Publications/index.jsx
+++ src/components/Publications/index.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import heart from "../../assets/heart.svg";
 import like from "../../assets/like.svg";
 import styles from "./styles.module.css";
 
-export default ({ title, author }) => {
+export default ({ title }) => {
+  const [numberOfHeart, setHeart] = useState(0)
+  const [numberOfLike, setLike] = useState(0)
+
+
+  const handleHeart = () => {
+    console.log("heart")
+  }
+
+  const handleLike = () => {
+    console.log("like")
+  }
+
+
   return (
     <div className={`${styles.publication_wrapper} p-4`}>
       <h3 className={`${styles.publication_title} pb-3`}>{title}</h3>
-      <p>{author}</p>
-
-      <img className="pe-3" src={heart} alt="amei" />
-      <img src={like} alt="gostei" />
+    
+   
+   <div className="d-flex">
+   <span className="d-flex align-items-center">
+    {numberOfHeart} <img onClick={handleHeart} className="ps-1 pe-3 cursor-pointer" src={heart} alt="amei" />
+    </span>
+     <span className="d-flex align-items-center">
+     {numberOfLike} <img onClick={handleLike} className="ps-1 cursor-pointer" src={like} alt="gostei" />
+     </span>
+   </div>
     </div>
   );
 };
--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import { get_posts } from "../../services";
 import { Container, Row, Col } from "reactstrap";
 import WritePublication from "../../components/WritePublication";
+import Publications from "../../components/Publications"
+import styles from "./styles.module.css"
+
+
 export default () => {
   const [publications, setPublications] = useState([]);
 
   const getPublication = async () => {
     const response = await get_posts();
-    console.log(response);
+    setPublications(response.data)
   };
   useEffect(() => {
     if (
@@ -26,7 +30,9 @@ export default () => {
           <WritePublication />
         </Col>
 
-        <Col sm={12} md={6}></Col>
+        <Col className={styles.publications_col} sm={12} md={6}>
+          {publications.length > 0 && publications.map(({content}, index) => <Publications key={index} title={content} />)}
+        </Col>
       </Row>
     </Container>
   );
